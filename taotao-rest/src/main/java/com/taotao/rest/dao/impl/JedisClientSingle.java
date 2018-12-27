27b82cb10d7301b9eeb3f038f7e2425cbe916279
haojiaxin <haojiaxin_axin@163.com>
2018-12-28 00:33:08 +0800

    向业务逻辑中添加缓存

---
+++ taotao-rest/src/main/java/com/taotao/rest/dao/impl/JedisClientSingle.java
@@ -0,0 +1,113 @@
+/**  
+ * All rights Reserved, Designed By www.tydic.com
+ * @Title:  JedisClientSingle.java   
+ * @Package com.taotao.rest.dao   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: axin     
+ * @date:   2018年12月28日 上午12:00:53   
+ * @version V1.0 
+ * @Copyright: 2018 www.hao456.top Inc. All rights reserved. 
+ */
+package com.taotao.rest.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.taotao.rest.dao.JedisClient;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+/**   
+ * @Description: TODO 
+ * @ClassName:  JedisClientSingle
+ * @author:  Axin 
+ * @date:   2018年12月28日 上午12:00:53   
+ * @Copyright: 2018 www.hao456.top Inc. All rights reserved. 
+ */
+//未使用@Repository注解 ，在spring中配置
+//@Repository
+public class JedisClientSingle implements JedisClient {
+	
+	//byType 在容器中找JedisPool类型，找到后注入进来,如果是根据byName匹配的话，配置文件的bean须要有一个id属性
+	//@Resource是jdk自带的注解 ,
+	@Autowired
+	private JedisPool jedisPool;
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#get(java.lang.String)
+	 */
+	@Override
+	public String get(String key) {
+		Jedis jedis = jedisPool.getResource();
+		String string = jedis.get(key);
+		jedis.close();
+		return string;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#set(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String set(String key, String value) {
+		Jedis jedis = jedisPool.getResource();
+		String string = jedis.set(key, value);
+		jedis.close();
+		return string;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#hget(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String hget(String hkey, String key) {
+		Jedis jedis = jedisPool.getResource();
+		String string = jedis.hget(hkey, key);
+		jedis.close();
+		return string;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#hset(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public long hset(String hkey, String key, String value) {
+		Jedis jedis = jedisPool.getResource();
+		Long result = jedis.hset(hkey, key, value);
+		jedis.close();
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#incr(java.lang.String)
+	 */
+	@Override
+	public long incr(String key) {
+		Jedis jedis = jedisPool.getResource();
+		Long result = jedis.incr(key);
+		jedis.close();
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#expire(java.lang.String, long)
+	 */
+	@Override
+	public long expire(String key, int second) {
+		Jedis jedis = jedisPool.getResource();
+		Long result = jedis.expire(key,second);
+		jedis.close();
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.taotao.rest.dao.JedisClient#ttl(java.lang.String)
+	 */
+	@Override
+	public long ttl(String key) {
+		Jedis jedis = jedisPool.getResource();
+		Long result = jedis.ttl(key);
+		jedis.close();
+		return result;
+	}
+
+}
--- taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
+++ taotao-rest/src/main/java/com/taotao/rest/service/impl/ContentServiceImpl.java
@@ -12,12 +12,16 @@ package com.taotao.rest.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import com.taotao.rest.dao.JedisClient;
 import com.taotao.rest.service.ContentService;
 
 /**   
@@ -33,16 +37,38 @@ public class ContentServiceImpl implements ContentService {
 	
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${INDEX_CONTENT_REDIS_KEY}")
+	private String INDEX_CONTENT_REDIS_KEY;
 	
 	/* (non-Javadoc)
 	 * @see com.taotao.rest.service.ContentService#getContentList(long)
 	 */
 	@Override
 	public List<TbContent> getContentList(long contentCid) {
-		
+		//从缓存中取内容
+		try {
+			String result = jedisClient.hget(INDEX_CONTENT_REDIS_KEY, contentCid+"");
+			if(!StringUtils.isBlank(result)){
+				//把字符串转换成list
+				List<TbContent> resultList = JsonUtils.jsonToList(result, TbContent.class);
+				return resultList;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		TbContentExample example = new TbContentExample();
 		example.createCriteria().andCategoryIdEqualTo(contentCid);
 		List<TbContent> list = this.contentMapper.selectByExample(example);
+		//向缓存中添加内容
+		try {
+			//需要把list转换成字符串
+			String string = JsonUtils.objectToJson(list);
+			jedisClient.hset("INDEX_CONTENT_REDIS_KEY", contentCid+"", string);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
